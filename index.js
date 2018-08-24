3ac7cc11258ae8a8315241ed66d4e2a0fcd58f09
Amory Hoste <amory.hoste@ugent.be>
2018-08-24 17:20:28 +0200

    add comments

--- index.js
+++ index.js
@@ -3,28 +3,32 @@ const fs = require('fs-extra');
 const tmp = require('tmp-promise');
 const path = require('path');
 
+// Used libraries
 const yarrrml_parser = './node_modules/@rmlio/yarrrml-parser/bin/cli.js';
 const rmlmapper = path.join(__dirname, 'rmlmapper-java/rmlmapper-java.jar');
 
 module.exports = function yarrrml2ld(options) {    
 
+    // Create temp file for generated rml
     let rml_file_tmp = tmp.file().then((file) => {
         return file.path;
     });
 
+    // Create temp file for generated linked data
     let linked_file_tmp = tmp.file().then((file) => {
         return file.path;
     });
 
-    // Convert yarrrml to rml
     return rml_file_tmp.then((rml_file_tmp) => {
         // Convert yarrrml to rml
         let executed = executeNodeScript(yarrrml_parser, ['-i', options.yarrrml_file, '-o', rml_file_tmp, '-f', 'RML']);
 
         return Promise.all([rml_file_tmp, linked_file_tmp, executed]);
+
     }).then(([rml_file_tmp, linked_file_tmp]) => {
         let arguments = ['-jar', rmlmapper, '-m', rml_file_tmp, '-o', linked_file_tmp, '-d'];
 
+        // Add functions file if necessary
         if (options.hasOwnProperty('functions')) {
             arguments.push('-f');
             arguments.push(options.functions);
@@ -32,12 +36,15 @@ module.exports = function yarrrml2ld(options) {
 
         // Convert rml to linked data
         return executeJar(linked_file_tmp, arguments);
+
     }).then((file_path) => {
         // Read all generated files and join them to a string
         return getLinkedData(file_path);
     });
 };
 
+// Looks if there are ntriples / nquads files present on the given path, joins them and returns their data
+// (the jarrrmlmapper-java adds a nq / nt extension to the file_path you passed)
 function getLinkedData(path) {
     let files = [];
     let nq_file = path + ".nq";
@@ -60,7 +67,7 @@ function getLinkedData(path) {
     });
 }
 
-// Transforms triple-pattern-fragment query into promise
+// Runs a node script and resolves when done
 function executeNodeScript(path, args) {
     return new Promise((resolve, reject) => {
 
@@ -78,6 +85,7 @@ function executeNodeScript(path, args) {
     })
 }
 
+// Runs a java jar and resolves when node
 function executeJar(outdir, args) {
     return new Promise((resolve, reject) => {
 
